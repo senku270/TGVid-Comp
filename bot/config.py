662cb3d7a8428b7175a0474b01919e700a5342fa
NexDaisongoing <125869769+NexDaisongoing@users.noreply.github.com>
2024-02-04 12:16:30 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,9 +4,9 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
+    APP_ID = config("24810254", cast=int)
+    API_HASH = config("aadb42caec01695fa0a77c09b3e0ef47")
+    BOT_TOKEN = config("6910156438:AAHblV1vwqJiVs1wo5oa9xTj8KUkQnGlf_g")
     DEV = 1664850827
     OWNER = config("OWNER")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
