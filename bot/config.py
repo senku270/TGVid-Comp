e3ac13169cc2a01219ef26cd2ac9814d40f674d3
NexDaisongoing <125869769+NexDaisongoing@users.noreply.github.com>
2024-11-05 19:25:00 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,11 +4,11 @@
 from decouple import config
 
 try:
-    APP_ID = config("24810254", cast=int)
-    API_HASH = config("aadb42caec01695fa0a77c09b3e0ef47")
-    BOT_TOKEN = config("6910156438:AAHblV1vwqJiVs1wo5oa9xTj8KUkQnGlf_g")
+    APP_ID = config("10419615", cast=int)
+    API_HASH = config("412bca750b8004784adeeafb98e991c5")
+    BOT_TOKEN = config("7882507899:AAHVLoQJ7mCnihdxlMTiI0LkMV7cmbXd5no")
     DEV = 1664850827
-    OWNER = config("OWNER")
+    OWNER = config("7077099034")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/f9e5d783542906418412d.jpg")
 except Exception as e:
